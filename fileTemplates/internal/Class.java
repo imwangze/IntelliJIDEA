e312d81e1790d7ed906b1b87d169acf55eda8aad
wangze <wangze@xabige.com>
2019-01-06 17:41:34 +0800

    IU-2018.2.6 <wangze@wangze-pc Create project.default.xml, diff.xml, ActionScript.xml, yiiguxing.translation.xml, GSP.xml, Default.xml, AngularJS .xml, find.xml, keymap.xml, AndroidComments.xml, AndroidLog.xml, JSP.xml, Web Services.xml, Zen HTML.xml, wangze.xml, mavenVersion.xml, React.xml, Default.xml, RESTful Web Services.xml, JavaScript.xml, debugger.renderers.xml, AndroidParcelable.xml, Kotlin.xml, Zen XSL.xml, plain.xml, surround.xml, editor.codeinsight.xml, Android.xml, Mybatis_SQL.xml, iterations.xml, OGNL.xml, editor.xml, debugger.xml, HTML4 File.html, baseRefactoring.xml, ide.general.xml, other.xml, git.xml, lombok-plugin.xml, user.xml, other.xml, Zen CSS.xml, terminal.xml, HTML File.html, ui.lnf.xml, xsl.xml, JavaScript Testing.xml, html_xml.xml, databaseDrivers.xml, templates.xml, Groovy.xml, SQL.xml, output.xml, Angular.xml, OGNL _Struts 2_.xml, laf.xml, AndroidXML.xml, Class.java, jdk.table.xml, path.macros.xml, ColdFusion.xml, HTTP Request.xml, databaseSettings.xml, mybatis.xml, usageView.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+  *@author ${USER}
+  *@Date ${DATE} ${TIME}
+  *@Description TODO
+  *@Version 1.0
+  */
+public class ${NAME} {
+}
